1257b79fb2df662b9dae5bc0d6f17bc14e434e0a
quinella1509 <hung15092004@gmail.com>
2024-11-14 11:52:58 +0700

    Initial commit

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then((data) => setMessage(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.txt
@@ -0,0 +1,6 @@
+# For Backend
+    cd Backend
+    node <StartingFile.js> (default "Index.js")
+# For Frontend
+    cd Frontend
+    npm start 
