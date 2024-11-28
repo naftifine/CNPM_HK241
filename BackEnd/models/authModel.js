305c0f1e9f624ec9eb750333a48e828a4ca08398
naftifine <hung15092004@gmail.com>
2024-11-29 03:59:00 +0700

    (BE): Added last_login

--- BackEnd/models/authModel.js
+++ BackEnd/models/authModel.js
@@ -2,9 +2,13 @@ const db = require('../db');
 
 const authModel = {
     findUserByBKNetID: (bknetid, password, callback) => {
-    const query = 'SELECT * FROM student WHERE bknetid = ? AND password = ?';
-    db.query(query, [bknetid, password], callback);
-  },
+        const query = 'SELECT * FROM student WHERE bknetid = ? AND password = ?';
+        db.query(query, [bknetid, password], callback);
+    },
+    updateLastLogin: (bknetid, callback) => {
+        const query = `UPDATE student SET last_login = DATE(NOW()) WHERE bknetid = ?`;
+        db.query(query, [bknetid], callback);
+    },
 };
 
 module.exports = authModel;
--- BackEnd/services/authService.js
+++ BackEnd/services/authService.js
@@ -2,20 +2,27 @@ const authModel = require('../models/authModel');
 
 const login = (bknetid, password, callback) => {
   if (!bknetid || !password) {
-    return callback({ status: 400, message: 'Vui lòng nhập BKNetID và mật khẩu.' }, null);
+      return callback({ status: 400, message: 'Vui lòng nhập BKNetID và mật khẩu.' }, null);
   }
 
   authModel.findUserByBKNetID(bknetid, password, (err, results) => {
-    if (err) {
-      return callback({ status: 500, message: 'Internal server error' }, null);
-    }
+      if (err) {
+          return callback({ status: 500, message: 'Internal server error' }, null);
+      }
 
-    if (results.length > 0) {
-      const { student_id, name } = results[0];
-      return callback(null, { success: true, student_id, name });
-    } 
-    else {
-      return callback({ status: 403, message: 'Sai BKNetID hoặc mật khẩu.' }, null);
+      if (results.length > 0) {
+          const { student_id, name } = results[0];
+          return callback(null, { success: true, student_id, name });
+      } 
+      else {
+          return callback({ status: 403, message: 'Sai BKNetID hoặc mật khẩu.' }, null);
+      }
+  });
+  authModel.updateLastLogin(bknetid, (err, result) => {
+    if (err) {
+      console.error('Error updating last login:', err);
+    } else {
+      console.log('Last login updated successfully');
     }
   });
 };
