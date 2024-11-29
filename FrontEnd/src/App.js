c95c1ef4f553830758c82d1d817ef05be890e826
vuducduy2210549 <duy.vuduc2210549@hcmut.edu.vn>
2024-11-29 23:42:06 +0700

    update components

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import PrinterManage from './pages/printerManage.js';
+
 
 function App() {
   const [message, setMessage] = useState('');
@@ -19,24 +23,15 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>{message}</p>
-      </header>
-    </div>
+    
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PrinterManage/>} />
+            
+          </Routes>
+      </BrowserRouter>
+    
   );
-}
+};
 
 export default App;
