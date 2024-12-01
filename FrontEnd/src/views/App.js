90ddc8b49e806c8043b847361d03809e22a95358
ThuyDuong <duong.tranduong2505@hcmut.edu.vn>
2024-12-01 23:25:00 +0700

    paymore

--- FrontEnd/src/views/App.js
+++ FrontEnd/src/views/App.js
@@ -10,6 +10,7 @@ import Choose_file from '../pages/choose_file';
 import AddNewPrinter from '../pages/addprinter';
 import Report from '../pages/Report';
 import History_print from '../pages/history_print';
+import Paymore from '../pages/paymore';
 function App() {
 
   return (
@@ -20,12 +21,13 @@ function App() {
         <Route path="/profile" element={<Inforstudent />} />
         <Route path="/print" element={<Choose_file />} />
         <Route path="/history" element={<History_print />} />
+        <Route path="/paymore" element={<Paymore />} />
         <Route path="/homespso" element={<Main_spso />} />
         <Route path="/printermanage" element={<PrinterManage />} />
         <Route path="/inforprinter/:printerid" element={<InfoPrinter />} />
         <Route path="/addnewprinter" element={<AddNewPrinter />} />
         <Route path="/inforprinter/:printerid" element={<InfoPrinter />} />
-        <Route path="/" element={<Report />} />
+        <Route path="/report" element={<Report />} />
       </Routes>
     </Router>
   )
